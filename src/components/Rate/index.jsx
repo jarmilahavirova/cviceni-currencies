c32c5a3bddd5de830b34680097da16ab07d30cba
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-05-12 21:37:16 +0200

    Stahování hodnot pomocí API

--- src/components/Rate/index.jsx
+++ src/components/Rate/index.jsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import "./style.css";
+import { useState } from "react";
 
 const currencies = {
   USD: { CZK: 23.823 },
@@ -7,11 +9,27 @@ const currencies = {
 };
 
 export const Rate = ({ from }) => {
+  const [rate, setRate] = useState(null);
+
+  useEffect(() => {
+    const checkRate = async () => {
+      const response = await fetch(
+        `https://api.frankfurter.app/latest?from=${from}&to=CZK`
+      );
+      const json = await response.json();
+      setRate(json.rates.CZK);
+    };
+
+    checkRate();
+  }, [from]);
+
   return (
     <div className="rate">
       <div className="rate__currency">1 {from}</div>
       <div>=</div>
-      <div className="rate__value">{currencies[from].CZK} CZK</div>
+      <div className="rate__value">
+        {rate === null ? "Načítám data" : `${rate} CZK`}
+      </div>
     </div>
   );
 };
